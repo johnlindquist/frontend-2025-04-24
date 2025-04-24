d3607f828ad63e53f069342dd7bacf0da924c3ae
John Lindquist <johnlindquist@gmail.com>
2025-04-24 12:26:00 -0600

    fix: Update next/link usage in Header to remove legacyBehavior and nested <a> tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,22 +6,22 @@ const Header: React.FC = () => {
   return (
     <header className="bg-black py-4 shadow-lg border-b-2 border-yellow-600">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <Link href="/" legacyBehavior>
-          <a className="text-4xl text-yellow-400 font-heading cursor-pointer">Deco Flights</a>
+        <Link href="/" className="text-4xl text-yellow-400 font-heading cursor-pointer">
+          Deco Flights
         </Link>
         <nav className="hidden md:flex space-x-6 font-heading text-lg">
-          <Link href="/#" legacyBehavior>
-            <a className="text-yellow-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition">Flights</a>
+          <Link href="/#" className="text-yellow-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition">
+            Flights
           </Link>
-          <Link href="/#hotels" legacyBehavior> {/* Assuming links might target sections or future pages */}
-            <a className="text-gray-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition">Hotels</a>
+          <Link href="/#hotels" className="text-gray-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition"> {/* Assuming links might target sections or future pages */}
+            Hotels
           </Link>
-          <Link href="/#packages" legacyBehavior>
-            <a className="text-gray-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition">Packages</a>
+          <Link href="/#packages" className="text-gray-400 hover:text-white border-b-2 border-transparent hover:border-yellow-400 transition">
+            Packages
           </Link>
         </nav>
         <div className="text-yellow-400">
-          <FaUserCircle size={32} /> {/* Use React Icon, adjusted size */} 
+          <FaUserCircle size={32} /> {/* Use React Icon, adjusted size */}
         </div>
       </div>
     </header>
